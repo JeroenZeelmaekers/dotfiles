d719f396969588d3b4ed4b07975c18d84cbf7d07
Jeroen Zeelmaekers <mail@jeroenzeelmaekers.com>
2024-04-11 19:42:15 +0200

    feat: add clear hlsearch cmd

--- nvim/lua/jeroen/core/keymaps.lua
+++ nvim/lua/jeroen/core/keymaps.lua
@@ -3,6 +3,8 @@ vim.g.maplocalleader = " "
 
 local keymap = vim.keymap
 
+keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear highlighted search" })
+
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
