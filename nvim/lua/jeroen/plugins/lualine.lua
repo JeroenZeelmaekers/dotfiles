83dc517f74fd8ac62c0068fc6029744cf4464444
Jeroen Zeelmaekers <mail@jeroenzeelmaekers.com>
2024-08-11 12:00:09 +0200

    feat(nvim): update lualine styling

--- nvim/lua/jeroen/plugins/lualine.lua
+++ nvim/lua/jeroen/plugins/lualine.lua
@@ -3,13 +3,15 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local lualine = require("lualine")
-		local lazy_status = require("lazy.status")
 		local lualine_gruvbox = require("lualine.themes.gruvbox")
 
 		lualine_gruvbox.normal.c.bg = "transparent"
 		lualine_gruvbox.visual.c.bg = "transparent"
+		lualine_gruvbox.visual.c.fg = "#a89984"
 		lualine_gruvbox.insert.c.bg = "transparent"
+		lualine_gruvbox.insert.c.fg = "#a89984"
 		lualine_gruvbox.command.c.bg = "transparent"
+		lualine_gruvbox.command.c.fg = "#a89984"
 		lualine_gruvbox.inactive.c.bg = "transparent"
 
 		lualine.setup({
@@ -17,16 +19,12 @@ return {
 				theme = lualine_gruvbox,
 			},
 			sections = {
-				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#ff9e64" },
-					},
-					{ "encoding" },
-					{ "fileformat" },
-					{ "filetype" },
-				},
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "mode" },
+				lualine_x = { "filename" },
+				lualine_y = {},
+				lualine_z = {},
 			},
 		})
 	end,
